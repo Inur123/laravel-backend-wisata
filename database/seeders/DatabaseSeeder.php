a8acac76f3a193a518fdd844648ba6dd33b64b72
Inur123 <zainurroziqin38@gmail.com>
2024-05-21 16:37:45 +0700

    zoom 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,10 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +16,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'FIC 17',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
         ]);
+
+        //category factory 2
+        Category::factory(20)->create();
+
+        //product factory 100
+        Product::factory(100)->create();
     }
 }
