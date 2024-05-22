c428c104a314e05599a27cd9ac534809dfce01e4
Inur123 <zainurroziqin38@gmail.com>
2024-05-22 17:51:35 +0700

    fix sidebar active

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -7,26 +7,25 @@
             <a href="index.html">St</a>
         </div>
         <ul class="sidebar-menu">
-            <li class="nav-item  ">
-                <a href="#" class="nav-link has-dropdown"><i
-                        class="fas fa-tachometer"></i><span>Dashboard</span></a>
+            <li class="nav-item {{ Request::routeIs('home') ? 'active' : '' }} ">
+                <a href="{{ route('home') }}" class="nav-link"><i class="fas fa-tachometer"></i><span>Dashboard</span></a>
 
             </li>
 
-            <li class="nav-item ">
+            <li class="nav-item {{ Request::routeIs('users.index') ? 'active' : '' }} ">
                 <a href="{{ route('users.index') }}" class="nav-link "><i class="fas fa-user"></i>
                     <span>Users</span></a>
             </li>
 
-            <li class="nav-item ">
+            <li class="nav-item {{ Request::routeIs('categories.index') ? 'active' : '' }} ">
                 <a href="{{ route('categories.index') }}" class="nav-link "><i class="fas fa-list-alt"></i>
                     <span>Categories</span></a>
             </li>
 
-            <li class="nav-item ">
+            <li class="nav-item {{ Request::routeIs('products.index') ? 'active' : '' }} ">
                 <a href="{{ route('products.index') }}" class="nav-link "><i class="fas fa-ticket"></i>
                     <span>Tickets</span></a>
             </li>
-
+        </ul>
     </aside>
 </div>
